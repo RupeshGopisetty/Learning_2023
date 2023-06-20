76328a48a7f26da6dc4bd1cd7d749b5449ebe2fd
Rupesh <rupeshgopisetty.92@gmail.com>
2023-06-20 16:44:13 +0530

    files added

--- Module1/Day3/day3q1.c
+++ Module1/Day3/day3q1.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
-void numbers(int n) {
-    int i;
-    for (i = 1; i <= n; i++) {
-        printf("%d", i);
-    }
-    for ( i = n - 1; i >= 1; i--) {
-        printf("%d", i);
-    }
-}
-void spaces(int n) {
-    int i;
-    for (i = 1; i <= n; i++) {
-        printf(" ");
-    }
-}
-void pattern(int n) {
-    int i;
-    for ( i = n; i >= 1; i--) {
-        numbers(i);
-        spaces(2 * (n - i));
-        numbers(i);
-        printf("\n");
-    }
-}
-int main() {
+
+void printDecreasing(int n);
+void printSpaces(int n);
+void printIncreasing(int n);
+
+int main()
+{
     int n;
     printf("Enter the value of n: ");
     scanf("%d", &n);
-
-    pattern(n);
-
+    
+    for (int i = n; i >= 1; i--)
+    {
+        printIncreasing(i);
+        printSpaces((n - i) * 2);
+        printDecreasing(i);
+        printf("\n");
+    }
+    
     return 0;
+}
+
+void printDecreasing(int n)
+{
+    for (int i = n; i >= 1; i--)
+    {
+        printf("%d", i);
+    }
+}
+
+void printSpaces(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf(" ");
+    }
+}
+
+void printIncreasing(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d", i);
+    }
 }
