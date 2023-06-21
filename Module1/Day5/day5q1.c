d7b6989a3f564d1b06d330c72d1c7df0eea7ab61
Rupesh <rupeshgopisetty.92@gmail.com>
2023-06-21 16:36:22 +0530

    files added

---
+++ Module1/Day5/day5q1.c
@@ -0,0 +1,33 @@
+// Program to toggle case
+#include<stdio.h>
+#include<ctype.h>
+
+void toggleCase(char* str)
+{
+    while(*str)
+    {
+        if(isupper(*str))
+            *str= tolower(*str);
+
+        else if(islower(*str))
+            *str= toupper(*str);
+    
+        str++;
+    }
+}
+
+int main() {
+    char str[] = "Hello World";
+    toggleCase(str);
+    printf("%s\n", str);
+
+    char str2[] = "A+B";
+    toggleCase(str2);
+    printf("%s\n", str2);
+
+    char str3[] = "Prog4u";
+    toggleCase(str3);
+    printf("%s\n", str3);
+
+    return 0;
+}
